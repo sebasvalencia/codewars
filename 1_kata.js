c99f4d290a7427fcf5e4d1b70db4c2b6bd0cc0e3
Sebastian Valencia <sebastian.valencian@gmail.com>
2017-12-07 11:29:23 -0500

    1_kata

---
+++ 1_kata.js
@@ -0,0 +1,23 @@
+/**
+
+Tasks
+
+mission 1:
+
+use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
+
+mission 2:
+
+use keyword var to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
+
+mission 3:
+
+type the console.log() in the next line(don't forget to put the str in the brackets).
+
+When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+ */
+function helloWorld() {
+    var str = "Hello World!";
+    console.log(str);
+}
