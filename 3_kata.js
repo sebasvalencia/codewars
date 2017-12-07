fb5b2bb5944979923bee8428b67eda778649d4dd
Sebastian Valencia <sebastian.valencian@gmail.com>
2017-12-07 13:24:15 -0500

    3_kata

---
+++ 3_kata.js
@@ -0,0 +1,44 @@
+/**
+Training JS #4: Basic data types--Array
+
+I've written five function, each function receives a parameter: arr(an array), you should coding something with arr.
+
+
+
+ */
+
+function getLength(arr){
+    //return length of arr
+    return ?
+  }
+  function getFirst(arr){
+    //return the first element of arr
+    return ?
+  }
+  function getLast(arr){
+    //return the last element of arr
+    return ?
+  }
+  function pushElement(arr){
+    var el=1;
+    //push el to arr
+    
+    return arr
+  }
+  function popElement(arr){
+    //pop an element from arr
+    
+    return ?
+  }
+
+
+
+
+
+
+
+
+
+
+
+
